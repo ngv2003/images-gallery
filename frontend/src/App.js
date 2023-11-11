2dc5b252aa9c9c3b935273dce2a64fa2ec0b8891
NGV <gowriviswanath3@gmail.com>
2023-11-11 09:28:18 +0530

    searchbar look done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log(e);
+}
+
+
 function App() {
   return (
     <div className="App">
-      <Header title="IMAGES GALLERY" />
+      <Header title="Images Gallery"/>
+      <Search handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar,Container} from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor: 'lightpink'
+};
 
 const Header = (props) => {
     return (
-        <Navbar bg="primary" data-bs-theme="dark">
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Navbar style={navbarStyle}  data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            </Container>
         </Navbar>
     )
 };
